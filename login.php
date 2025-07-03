003ad8177b95c589c0f81f858c431d562d90442e
ainunmulyana <76513223+ainunmulyana3748@users.noreply.github.com>
2025-07-03 10:20:47 +0700

    edit title login page

--- login.php
+++ login.php
@@ -58,7 +58,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     <head>
         <meta charset="utf-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <title>Login | My Daily Journal</title>
+        <title>Welcome to my Daily Journal</title>
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet"
             integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous" />
         <link rel="stylesheet"
